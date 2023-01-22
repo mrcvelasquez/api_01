fe16a658a8c6fbda9b7bdc4b9d368fb8e57afd1e
mrcvelasquez <74612833+mrcvelasquez@users.noreply.github.com>
2023-01-22 18:09:44 -0300

    Update api_01.py

--- api_01.py
+++ api_01.py
@@ -2,9 +2,8 @@ import requests
 
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cotacoes = cotacoes.json()
-cotacao_dolar = cotacoes ['USDBRL']["bid"]
-print(cotacao_dolar)
-
+cotacao_dolar = float(cotacoes ['USDBRL']["bid"])
+print(f'U$$ {cotacao_dolar:.2f}')
 
 
 
